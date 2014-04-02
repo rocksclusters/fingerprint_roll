6034c020152b3714a4940168bd15f3e7656cfa28
Luca Clementi <luca.clementi@gmail.com>
2014-04-01 18:05:46 -0700

    first version of the fingerprint roll
    it uses fingerprint v0.2

---
+++ src/fingerprint/version.mk
@@ -0,0 +1,4 @@
+PKGROOT		= /opt/fingerprint
+NAME        = fingerprint
+VERSION     = 0.2
+RELEASE 	= 1
---
+++ version.mk
@@ -0,0 +1,6 @@
+ROLLNAME	= fingerprint
+VERSION		= 1.0
+RELEASE		= 0
+COLOR		= moccasin
+
+REDHAT.ROOT	= $(CURDIR)
